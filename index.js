54ce1c552279d046c47af5e21d7ab9a58a1711a0
5late <markandmichfamily@gmail.com>
2020-08-15 10:12:35 -0400

    First Commit

--- index.js
+++ index.js
@@ -18,18 +18,21 @@ client.on('message', message => {
  message.reply('Deletus');
  }
 
+ if (message.content === 'Thanks Yoinkbot')
+ message.reply('No problem! My creator is Xurxx#7879, you can visit his website at https://5late.github.io')
+
  if (message.content === 'pog') {
 	message.channel.send('cheers ill drink to that').then(message.react('725376490836852766'));
 	
 }
 
-  if (message.content == '@Sweat') {
+  if (message.content === 'bruh') {
  
 	message.delete();
 
  if (message.content === 'Crab Rave') {
  
- 	message.react('\:crab:')
+ 	message.react(':crab:')
 	 }
 	}	 
 
@@ -37,7 +40,64 @@ client.on('message', message => {
 	 message.channel.send({files: ["https://i.imgur.com/O0QUXuj.gif"]} )
 	 
 }
- } 
- );
+
+if (message.content === 'fuck you say to me') {
+  message.delete();
+	message.channel.send({files: ["./ninjarage.mp4"] })
+	
+}
+
+if (message.content === 'im out') {
+  message.reply("You said...");
+	message.channel.send({files: ["./Fuck This Shit I'm Out.mp4"] })
+	
+}
+
+
+ if (message.content === 'rickroll') {
+	 setUsername(yeet);
+ }
+
+  if (message.content === 'what is my avatar') {
+    // Send the user's avatar URL
+    message.reply(message.author.displayAvatarURL());
+  }
+  
+  if (!message.guild) return;
+
+  if (message.content.startsWith('!kick')) {
+	
+const user = message.mentions.users.first();
+
+    if (user) {
+      // Now we get the member from the user
+      const member = message.guild.member(user);
+      // If the member is in the guild
+      if (member) {
+        member
+          .kick('Optional reason that will display in the audit logs')
+          .then(() => {
+            // We let the message author know we were able to kick the person
+            message.reply(`Successfully kicked ${user.tag}`);
+          })
+          .catch(err => {
+            // An error happened
+            // This is generally due to the bot not being able to kick the member,
+            // either due to missing permissions or role hierarchy
+            message.reply('I was unable to kick the member');
+            // Log the error
+            console.error(err);
+          });
+      } else {
+        // The mentioned user isn't in this guild
+        message.reply("That user isn't in this guild!");
+      }
+      // Otherwise, if no user was mentioned
+    } else {
+      message.reply("You didn't mention the user to kick!");
+    }
+ 
+}
+});
 
 client.login('NzI0OTg5MTU4ODkxODQ3NzAw.XvPOXQ.yy_Ha6JsvTjJz2gipfEPZkVouwM');
