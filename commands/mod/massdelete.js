3e52c4aa0bf05a441fe5217adfe0bbd9d47aa251
5late <markandmichfamily@gmail.com>
2020-09-02 22:28:49 -0400

    Added Discord code Embed messages for Bulk Delete error codes

--- commands/mod/massdelete.js
+++ commands/mod/massdelete.js
@@ -19,10 +19,10 @@ module.exports = class Massdelete extends commando.Command {
         const args = msg.content.split(' ').slice(1); 
         const amount = args.join(' ');
         {
-            if (!amount) return msg.reply('You have not given the amount of messages to delete you idiot.')
-            if (isNaN(amount)) return msg.reply('You absolute idiot numbers need to be numbers'); 
-            if (amount > 100) return msg.reply('Try not to kill this server, eh? Maximum number of messages to delete is 100'); 
-            if (amount < 1) return msg.reply('Are you even trying to delete anything?');
+            if (!amount) return msg.reply('You have not given the amount of messages to delete you idiot. ```Error Code 1: No message amount given.```')
+            if (isNaN(amount)) return msg.reply('You absolute idiot numbers need to be numbers. ```Error Code 2: Message amount is not a number.```'); 
+            if (amount > 100) return msg.reply('Try not to kill this server, eh? Maximum number of messages to delete is 100. ```Error Code 3: Message amount over 100.```'); 
+            if (amount < 1) return msg.reply('Are you even trying to delete anything? ```Error code 4: Message amount under 1.```');
             await msg.channel.messages.fetch({ limit: amount }).then(messages => { // Fetches the messages
                 msg.channel.bulkDelete(messages
             )});
