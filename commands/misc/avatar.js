c16bfa8edee562facef7ddcd8908e65febbe9858
5late <christianrfernandes5@gmail.com>
2021-04-18 11:38:59 -0400

    removed garbage

--- commands/misc/avatar.js
+++ commands/misc/avatar.js
@@ -9,7 +9,7 @@ module.exports = class OneThousandCommand extends commando.Command {
     constructor(client) {
         super(client, {
             name: 'avatar',
-            aliases: ['pfp'],
+            aliases: ['pfp', 'av', 'profilepicture'],
             group: 'misc',
             memberName: 'avatar',
             description: 'Displays your avatar',
@@ -46,26 +46,5 @@ module.exports = class OneThousandCommand extends commando.Command {
             .setColor('#275BF0')
             msg.channel.send(embed)
         }
-
-        const id = msg.author.id
-        console.log(id)
-        const name = msg.member.user.tag;
-        console.log(name)
-        var information = [];
-        information.push(name, id)
-        var savedinfo = fs.readFileSync("./information.txt", {"encoding": "utf-8"});
-
-      var newinfo = savedinfo;
-    fs.writeFileSync("information.txt", newinfo.toString())
-    
-    fs.appendFileSync("information.txt", information.toString())
-
-    fs.readFile("./information.txt", function (err, data) {
-        if (err) throw err;
-        var datata = data.toString('utf-8')
-        if(data.includes('Xurxx#7879')){
-         console.log((datata.length / 29) - .689655172413794)
-        }
-      });
     }
 }
