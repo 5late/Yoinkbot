266e4e1b8e63cf78578410e67b0efd867405f2ca
5late <christianrfernandes5@gmail.com>
2021-04-18 13:04:03 -0400

    updated catfact to actually work

--- commands/misc/catfact.js
+++ commands/misc/catfact.js
@@ -15,19 +15,17 @@ module.exports = class JokeCommand extends commando.Command {
             description: 'Sends a dad joke',
         })
     }
-    async run (msg) {
-        
-        //const args = msg.content.slice(prefix.length).trim().split(/ +/g);
-        //const command = args.shift().toLowerCase();
-        //const igUsername = args[0].toLowerCase();
-        
-        let getJoke = async () => {
-            let response = await fetch(
-                "https://cat-fact.herokuapp.com/facts"
-            )
-            console.log(response)
-        };
-        let jokeValue = await getJoke();
-        await msg.channel.send(jokeValue.text + ' | React with ▶️ to send punchline!')
+async run (msg) {
+    
+    function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
     }
+
+
+    await fetch("https://cat-fact.herokuapp.com/facts").then(response => response.json()).then(data => {
+        console.log(data)
+        let number = getRandomInt(5)
+        msg.channel.send(data[number].text)
+    })
+}
 }
