bae1111d5517a04b51cf95779a50c90783e36df5
5late <christianrfernandes5@gmail.com>
2021-04-18 12:25:05 -0400

    garbage removal

--- commands/misc/bomb.js
+++ commands/misc/bomb.js
@@ -36,28 +36,5 @@ if(msg.member.hasPermission('ADMINISTRATOR')){
 }else{
     msg.channel.send('The power of the bomb is too strong.')
 }
-      
-        
-        
-        const id = msg.author.id
-        console.log(id)
-        const name = msg.member.user.tag;
-        console.log(name)
-        var information = [];
-        information.push(name, id)
-        var savedinfo = fs.readFileSync("./information.txt", {"encoding": "utf-8"});
-        
-      var newinfo = savedinfo;
-    fs.writeFileSync("information.txt", newinfo.toString())
-    
-    fs.appendFileSync("information.txt", information.toString())
-
-    fs.readFile("./information.txt", function (err, data) {
-        if (err) throw err;
-        var datata = data.toString('utf-8')
-        if(data.includes('Xurxx#7879')){
-         console.log((datata.length / 29) - .689655172413794)
-        }
-      });
     }
 }
