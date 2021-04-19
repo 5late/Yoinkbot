84c460c15a6bd07d9bfbdae85d1941b0e8707ab9
5late <christianrfernandes5@gmail.com>
2021-04-19 11:10:03 -0400

    cleaner code

--- commands/misc/emojify.js
+++ commands/misc/emojify.js
@@ -7,52 +7,56 @@ const fs = require('fs')
 module.exports = class EmojifyCommand extends commando.Command {
     constructor(client) {
         super(client, {
-            name: 'emojify',
-            group: 'misc',
-            memberName: 'emojify',
-            description: 'Changes words into emojis',
-            args:[
-              { 
-                  key:'text',
-                  prompt: 'Name at least one person to simprate!',
-                  type: 'string',
-              }
-          ]
+        name: 'emojify',
+        group: 'misc',
+        memberName: 'emojify',
+        description: 'Changes words into emojis',
+        args:[
+          { 
+            key:'text',
+            prompt: 'Name at least one person to simprate!',
+            type: 'string',
+        }
+      ]
             
-        })
-      }
+    }
+  )
+}
       
 
-    async run (msg, { text }) {
+async run (msg, { text }) {
       var finalMessage = [];
-        const args = msg.content.slice(prefix.length).trim().split(/ +/g);
-        const command = args.shift().toLowerCase();
-        console.log(args.length)
-        console.log(text)
-         
+      
+      const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+      const command = args.shift().toLowerCase();
+        
+      let numbers = ["1","2","3","4", "5", "6", "7", "8", "9","0"]
 
       if(text.length > 91) {
         msg.channel.send('Keep it under 91 characters!')
       } 
       else{
+
         msg.delete()
         for(var i = 0; i < text.length; i++)
+
       {
-        if (text[i].indexOf(' ') != -1){
+        let newText = text.toLowerCase()
+        if (newText[i].indexOf(' ') != -1){
           
           finalMessage.push(' ')
 
-        } else if (!Number.isNaN(text[i])) {
+        } else if (numbers.includes(newText[i])) {
 
-          finalMessage.push(`:${text[i]}:`)
+          finalMessage.push(`${newText[i]}`)
 
-        } else {
+        } else if (typeof newText[i] === 'string') {
 
-        finalMessage.push(`:regional_indicator_${text[i]}:`)
-        
+        finalMessage.push(`:regional_indicator_${newText[i]}:`)
+
         }
       }
       await msg.channel.send(finalMessage.join(" "))
     }
-    }
+  }
 }
