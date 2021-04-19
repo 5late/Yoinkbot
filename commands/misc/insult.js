2b5c2d7f89af4c015aeb05486d580943d159f2c4
5late <christianrfernandes5@gmail.com>
2021-04-19 10:29:31 -0400

    cleanup

--- commands/misc/insult.js
+++ commands/misc/insult.js
@@ -21,12 +21,12 @@ module.exports = class InsultCommand extends commando.Command {
 async run (msg, { text }) {
       const args = msg.content.slice(prefix.length).trim().split(/ +/g);
       const command = args.shift().toLowerCase();
-    const trumpImage =['You are proof that God has a sense of humour', 'You are as useless as the ueue in queue', 'Mirrors cant talk, lucky for you, they cant laugh either.','You are the reason the gene pool needs a lifeguard', 'If I had a face like yours I would sue my parents', 'Someday, you will go far, adn I hope you stay there.', 'If laughter is medicine, your face must be the most curing in the world.', 'So, a thought justr crossed your mind? Must have been a long and lonely journey.', 'If I wanted to kill myself I would climb your ego and jump into your IQ','When you were born, the doctor threw you out the window and the window threw you back']
-    const image = trumpImage[Math.floor(Math.random() * trumpImage.length)];
+    const insults =['You are proof that God has a sense of humour', 'You are as useless as the ueue in queue', 'Mirrors cant talk, lucky for you, they cant laugh either.','You are the reason the gene pool needs a lifeguard', 'If I had a face like yours I would sue my parents', 'Someday, you will go far, and I hope you stay there.', 'If laughter is medicine, your face must be the most curing in the world.', 'So, a thought just crossed your mind? Must have been a long and lonely journey.', 'If I wanted to kill myself I would climb your ego and jump into your IQ','When you were born, the doctor threw you out the window and the window threw you back']
+    const insult = insults[Math.floor(Math.random() * insults.length)];
 
       const embed = new Discord.MessageEmbed()
       .setTitle('Insult machine.')
-      .setDescription(text + image)
+      .setDescription(text + insult)
 
       msg.channel.send(embed)
     }
