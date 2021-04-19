cfcaa0ea0f305565f9c9f1b4695c4c41dc760f9b
5late <christianrfernandes5@gmail.com>
2021-04-19 10:19:49 -0400

    added id support to compliment

--- commands/misc/compliment.js
+++ commands/misc/compliment.js
@@ -1,8 +1,8 @@
 const commando = require('discord.js-commando');
-const fs = require('fs');
-const { DiscordAPIError } = require('discord.js');
 const Discord = require('discord.js')
-const prefix  = '?'
+const prefix = '?'
+const path = require('path');
+
 module.exports = class OneThousandCommand extends commando.Command {
     constructor(client) {
         super(client, {
@@ -12,32 +12,40 @@ module.exports = class OneThousandCommand extends commando.Command {
             description: 'Compliment someone! Defaults to self.',
         })
     }
-async run (msg, { text }) {
+async run (msg) {
       
         const args = msg.content.slice(prefix.length).trim().split(/ +/g);
         const command = args.shift().toLowerCase();
 
-    const trumpImage =['You are a gift to others', 'I appreciate you', 'You deserve a hug right now.','You light up the room', 'You light up a room', 'You are an awesome friend', 'You bring out the best in people', 'You are making difference.', 'I may be a bot, but I am inspired by you.','On a scale from one to ten, you are an eleven.', 'You are more helpful than you realize.']
-    const image = trumpImage[Math.floor(Math.random() * trumpImage.length)];
+    const compliments =['You are a gift to others', 'I appreciate you', 'You deserve a hug right now.','You light up the room', 'You light up a room', 'You are an awesome friend', 'You bring out the best in people', 'You are making difference.', 'I may be a bot, but I am inspired by you.','On a scale from one to ten, you are an eleven.', 'You are more helpful than you realize.']
+    const compliment = compliments[Math.floor(Math.random() * compliments.length)];
 
 if(!args.length){
-        const person = msg.author.username;
-        
     const embed = new Discord.MessageEmbed()
-    //.setTitle('Compliment machine.')
       .setTitle(msg.author.username + image)
  
       msg.channel.send(embed)
     
 } else {
-    const person = msg.members. this.client.users.fetch(args);
+    function getIdFromMention(mention) {
+        if (!mention) return;
+
+        if (mention.startsWith('<@') && mention.endsWith('>')) {
+            mention = mention.slice(2, -1);
+            
+            if (mention.startsWith('!')) {
+                mention = mention.slice(1);
+            }
+    
+        }
+        return mention;
+    }
+    const person =  getIdFromMention(args[0]);
     const embed = new Discord.MessageEmbed()
       .setTitle('Compliment machine.')
-      //.setImage(image)
-      .setDescription(person + ' ' + image)
-      //.setFooter("Yoinkbot collects your username and tag to improve our services. To find out whats being collected contact the bot owner with the command '?owner'")
+      .setDescription(`<@${person}> ${compliment}`)
 
       msg.channel.send(embed)
 }
-    }
+}
 }
