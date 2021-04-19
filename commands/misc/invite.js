b0a3e6c338af220c860aa9a349d30624a2807886
5late <christianrfernandes5@gmail.com>
2021-04-19 11:23:42 -0400

    refreshed code

--- commands/misc/invite.js
+++ commands/misc/invite.js
@@ -16,16 +16,15 @@ module.exports = class OneThousandCommand extends commando.Command {
         })
       }
 async run (msg) {
-           
-       
+    
+  const embed = new Discord.MessageEmbed()
+    .setTitle('Invite Yoinkbot to your server!')
+    .setDescription('Did you know we have a beta bot? It gets bleeding edge updates before Yoinkbot does!')
+    .addField('Yoinkbot Link ⬇️', '[Click Here](https://discord.com/oauth2/authorize?client_id=724989158891847700&permissions=2147483639&scope=bot)')
+    .addField('Yoinkbot Beta Link ⬇️', '[Click Here](https://discord.com/api/oauth2/authorize?client_id=744940398006370444&permissions=8&scope=bot)')
+    .setFooter('Owner: Xurxx#7879 | ?owner')
 
-        const embed = new Discord.MessageEmbed()
-            .setTitle('Invite Yoinkbot to your server!')
-            .setDescription('Did you know we have a beta bot? It gets bleeding edge updates before Yoinkbot does!')
-            .addField('Yoinkbot Link ⬇️', '[Click Here](https://discord.com/oauth2/authorize?client_id=724989158891847700&permissions=2147483639&scope=bot)')
-            .addField('Yoinkbot Beta Link ⬇️', '[Click Here](https://discord.com/api/oauth2/authorize?client_id=744940398006370444&permissions=8&scope=bot)')
-            .setFooter('Owner: Xurxx#7879 | ?owner')
-
-        msg.channel.send(embed)
+  msg.channel.send(embed)
+  msg.channel.send("||The beta bot may appear offline often, due to it being for testing. Contact the bot owner ``?owner`` for more information.||")
   }
 }
