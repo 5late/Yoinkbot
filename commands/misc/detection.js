02ae301e927ba71fa672b10e81e0ec9b4da5a7d7
5late <christianrfernandes5@gmail.com>
2021-04-19 10:22:37 -0400

    garbage removal

--- commands/misc/detection.js
+++ commands/misc/detection.js
@@ -44,22 +44,5 @@ module.exports = class IdiotCommand extends commando.Command {
             .setFooter("Yoinkbot collects your usernames and tag to improve our services. To find out what's being collected, contact the bot owner with the command '?owner")
             msg.channel.send(newembed)
         }
-        
-        
-    var information = [];
-    information.push(name, id)
-    var savedinfo = fs.readFileSync("./information.txt", {"encoding": "utf-8"});
-        
-      var newinfo = savedinfo;
-    fs.writeFileSync("information.txt", newinfo.toString())
-    
-    fs.appendFileSync("information.txt", information.toString())
-    fs.readFile("./information.txt", function (err, data) {
-        if (err) throw err;
-        var datata = data.toString('utf-8')
-        if(data.includes('Xurxx#7879')){
-         console.log((datata.length / 29) - .689655172413794)
-        }
-      });
     }
 }
