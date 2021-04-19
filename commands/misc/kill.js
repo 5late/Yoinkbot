b9a3d03b272915faf08446972a9511720db5c252
5late <christianrfernandes5@gmail.com>
2021-04-19 11:27:53 -0400

    cleaner

--- commands/misc/kill.js
+++ commands/misc/kill.js
@@ -19,20 +19,17 @@ module.exports = class OneThousandCommand extends commando.Command {
           ]
         })
     }
-    async run (msg, { text }) {
+async run (msg) {
       const args = msg.content.slice(prefix.length).trim().split(/ +/g);
       const command = args.shift().toLowerCase();
-      const ruser = msg.author;
-      const rMember = msg.mentions.users.first();
-const trumpImage =[` pushed a cactus up ${rMember} nose`,` peeled ${rMember} fingernails off one by one`,  `spooned ${rMember} brain out and ate it`, `  poured bleach into ${rMember} eyeballs`, ` flipped ${rMember} head 180`,]
-const image = trumpImage[Math.floor(Math.random() * trumpImage.length)];
 
-//const responses = ['Drink Bleach!', 'Fake NEWS!', 'VOTE!!!!!', 'CHINA!', 'CORRUPT!!!!!', 'MAKE AMERICA GREAT AGAIN!!!', 'OBAMAGATE!!!!', 'You are the enemy of the people.', 'LAW AND ORDER!!!!', 'RIGGED ELECTION!!!', 'CROOKED HILLARY!!', 'SLEEPY JOE!!']
-//const response = responses[Math.floor(Math.random()* responses.length)];
-console.log(args)
+      const rMember = msg.mentions.users.first();
+      
+      const trumpImage =[` pushed a cactus up ${rMember} nose`,` peeled ${rMember} fingernails off one by one`,  `spooned ${rMember} brain out and ate it`, `  poured bleach into ${rMember} eyeballs`, ` flipped ${rMember} head 180`,]
+      const image = trumpImage[Math.floor(Math.random() * trumpImage.length)];
+
       const embed = new Discord.MessageEmbed()
       .setTitle('Kill machine.')
-      //.setImage(image)
       .setDescription(msg.author.username+image)
 
       msg.channel.send(embed)
