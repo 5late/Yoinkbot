c2d79475fb2f6faabe78408f59aab5e641fd544e
5late <christianrfernandes5@gmail.com>
2021-04-20 08:50:09 -0400

    garbage removal

--- commands/misc/linkshorter.js
+++ commands/misc/linkshorter.js
@@ -14,39 +14,15 @@ module.exports = class LinkShortenCommand extends commando.Command {
             description: 'Shorten a link. Works like bit.ly',
         })
     }
-    async run (msg, { text }) {
+async run (msg, { text }) {
       const args = msg.content.slice(prefix.length).trim().split(/ +/g);
       const command = args.shift().toLowerCase();
-      
-      
+    
       TinyURL.shorten(args.toString(), function(res, err) {
         if (err)
           console.log(err)
           console.log(res);
           msg.channel.send(res)
-      });
-
-
-
-        const id = msg.author.id
-        console.log(id)
-        const name = msg.member.user.tag;
-        console.log(name)
-        var information = [];
-        information.push(name, id)
-        var savedinfo = fs.readFileSync("./information.txt", {"encoding": "utf-8"});
-        
-      var newinfo = savedinfo;
-    fs.writeFileSync("information.txt", newinfo.toString())
-    
-    fs.appendFileSync("information.txt", information.toString())
-
-    fs.readFile("./information.txt", function (err, data) {
-        if (err) throw err;
-        var datata = data.toString('utf-8')
-        if(data.includes('Xurxx#7879')){
-         console.log((datata.length / 29) - .689655172413794)
-        }
       });
     }
 }
