162aff18dc3a3042b8445067241325a12c3c784d
5late <christianrfernandes5@gmail.com>
2021-04-20 08:46:07 -0400

    garbage removal

--- commands/misc/kiss.js
+++ commands/misc/kiss.js
@@ -25,26 +25,5 @@ module.exports = class OneThousandCommand extends commando.Command {
       const rMember = msg.mentions.users.first();
 
        msg.channel.send(msg.author.username + ' gave ' + text + ' a big smooch on the cheek 😗😗😘😘')
-
-        const id = msg.author.id
-        console.log(id)
-        const name = msg.member.user.tag;
-        console.log(name)
-        var information = [];
-        information.push(name, id)
-        var savedinfo = fs.readFileSync("./information.txt", {"encoding": "utf-8"});
-        
-      var newinfo = savedinfo;
-    fs.writeFileSync("information.txt", newinfo.toString())
-    
-    fs.appendFileSync("information.txt", information.toString())
-
-    fs.readFile("./information.txt", function (err, data) {
-        if (err) throw err;
-        var datata = data.toString('utf-8')
-        if(data.includes('Xurxx#7879')){
-         console.log((datata.length / 29) - .689655172413794)
-        }
-      });
-    }
+}
 }
