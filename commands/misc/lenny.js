860d620a5dfcfc6a254e9c1240acd38e103d5168
5late <christianrfernandes5@gmail.com>
2021-04-20 08:48:06 -0400

    cleaned up lenny command

--- commands/misc/lenny.js
+++ commands/misc/lenny.js
@@ -10,37 +10,21 @@ module.exports = class OneThousandCommand extends commando.Command {
             group: 'misc',
             memberName: 'lenny',
             description: 'Lenny',
-        })
+            aliases: [
+              'smiley'
+            ]
     }
-    async run (msg, { text }) {
+  )
+}
+    
+async run (msg, { text }) {
       const args = msg.content.slice(prefix.length).trim().split(/ +/g);
       const command = args.shift().toLowerCase();
-const lenny =['( ͡° ͜ʖ ͡°)', '( ͠° ͟ʖ ͡°)', '( ͡~ ͜ʖ ͡°)', '( ͡ʘ ͜ʖ ͡ʘ)', '( ͡o ͜ʖ ͡o)', '(° ͜ʖ °)', '( ‾ʖ̫‾)', '( ಠ ͜ʖಠ)', '( ͡° ʖ̯ ͡°)', '( ͡ಥ ͜ʖ ͡ಥ)', '༼  ͡° ͜ʖ ͡° ༽', '(▀̿Ĺ̯▀̿ ̿)', '( ✧≖ ͜ʖ≖)', '(ง ͠° ͟ل͜ ͡°)ง', '(͡ ͡° ͜ つ ͡͡°)' , '[̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅]', '(✿❦ ͜ʖ ❦)', 'ᕦ( ͡° ͜ʖ ͡°)ᕤ', '( ͡° ͜ʖ ͡°)╭∩╮', '¯\_( ͡° ͜ʖ ͡°)_/¯', '(╯ ͠° ͟ʖ ͡°)╯┻━┻', '( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°)', '¯\_(ツ)_/¯', 'ಠ_ಠ', '(个_个)']
-const lennyimage = lenny[Math.floor(Math.random() * lenny.length)];
+    
+    const lenny =['( ͡° ͜ʖ ͡°)', '( ͠° ͟ʖ ͡°)', '( ͡~ ͜ʖ ͡°)', '( ͡ʘ ͜ʖ ͡ʘ)', '( ͡o ͜ʖ ͡o)', '(° ͜ʖ °)', '( ‾ʖ̫‾)', '( ಠ ͜ʖಠ)', '( ͡° ʖ̯ ͡°)', '( ͡ಥ ͜ʖ ͡ಥ)', '༼  ͡° ͜ʖ ͡° ༽', '(▀̿Ĺ̯▀̿ ̿)', '( ✧≖ ͜ʖ≖)', '(ง ͠° ͟ل͜ ͡°)ง', '(͡ ͡° ͜ つ ͡͡°)' , '[̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅]', '(✿❦ ͜ʖ ❦)', 'ᕦ( ͡° ͜ʖ ͡°)ᕤ', '( ͡° ͜ʖ ͡°)╭∩╮', '¯\_( ͡° ͜ʖ ͡°)_/¯', '(╯ ͠° ͟ʖ ͡°)╯┻━┻', '( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°)', '¯\_(ツ)_/¯', 'ಠ_ಠ', '(个_个)']
+    const lennyimage = lenny[Math.floor(Math.random() * lenny.length)];
 
       msg.delete()
       msg.channel.send(lennyimage)
-        
-        
-        const id = msg.author.id
-        console.log(id)
-        const name = msg.member.user.tag;
-        console.log(name)
-        var information = [];
-        information.push(name, id)
-        var savedinfo = fs.readFileSync("./information.txt", {"encoding": "utf-8"});
-        
-      var newinfo = savedinfo;
-    fs.writeFileSync("information.txt", newinfo.toString())
-    
-    fs.appendFileSync("information.txt", information.toString())
-
-    fs.readFile("./information.txt", function (err, data) {
-        if (err) throw err;
-        var datata = data.toString('utf-8')
-        if(data.includes('Xurxx#7879')){
-         console.log((datata.length / 29) - .689655172413794)
-        }
-      });
-    }
+  }
 }
