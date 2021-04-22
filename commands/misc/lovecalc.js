445aca7e3cbbe5a590e65815d851ede118e3faba
5late <christianrfernandes5@gmail.com>
2021-04-22 19:31:03 -0400

    new lovecalc command!

--- commands/misc/lovecalc.js
+++ commands/misc/lovecalc.js
@@ -1,10 +1,8 @@
 const commando = require('discord.js-commando');
-const { DiscordAPIError } = require('discord.js');
 const Discord = require('discord.js');
-const client = new commando.CommandoClient;
-const fs = require('fs')
 const prefix = '?'
-module.exports = class SimprateCommand extends commando.Command {
+
+module.exports = class LoveCalcCommand extends commando.Command {
     constructor(client) {
         super(client, {
             name: 'lovecalc',
@@ -22,24 +20,34 @@ module.exports = class SimprateCommand extends commando.Command {
     }
     
 async run (msg, { text }) {
-        let simprate = Math.floor(Math.random() * 100)
-        
+    
+    function CalculateColor(percent){
+        let color = `#ddd`
+        if(percent > 10 && percent < 20) {
+            color = `#f54542`
+        } 
+        else if (percent > 20 && percent < 40) {
+            color = `#f58742`
+        }
+        else if (percent > 40 && percent < 60) {
+            color = `#f5cb42`
+        }
+        else if (percent > 60 && percent < 80) {
+            color = `#b2f743`
+        }
+        else if (percent > 80 && percent < 100) {
+            color = `#35fc3f`
+        }
+        return color
+    }
+
+    let lovecalc = Math.floor(Math.random() * 100)
         
     const newembed = new Discord.MessageEmbed()
-        .setColor('#00FFCC')
+        .setColor(CalculateColor(lovecalc))
         .setTitle('Cupid')
-        .addField("Result:", `${text}` + ' you are ' + simprate + '% in love! 💔💔💔')
-        .setFooter("Yoinkbot collects your username and tag to improve our services. To find out whats being collected, contact the bot owner with the command '?owner'")
-    if(simprate >50) {
-    const newerembed = new Discord.MessageEmbed()
-        .setColor('#00FF2E')
-        .setTitle('Simprating Machine')
-        .setDescription(`${text}` + ' you are ' + simprate + ' % in love! 😍😍😍')
-        .setFooter("Yoinkbot collects your username and tag to improve our services. To find out whats being collected, contact the bot owner with the command '?owner'")
-    msg.channel.send(newerembed);
-    }
-    else {
+        .addField("Result:", `${text}` + ' you are ' + lovecalc + '% in love!')
+        .setFooter("Yoinkbot")
     msg.channel.send(newembed)
-    }
 }
 }
