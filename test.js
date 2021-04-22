e4aa355021348e23a3bed390fe526d49447ce863
5late <christianrfernandes5@gmail.com>
2021-04-22 19:08:07 -0400

    added a better test js system

--- test.js
+++ test.js
@@ -1,5 +1,17 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const fs = require('fs')
 
-client.login(DISCORD_TOKEN)
-console.log("logged in!")
+const path1 = './index.js'
+const path2 = './package.json'
+const path3 = './package-lock.json'
+const path4 = './build'
+const path5 = './commands'
+
+for (var i = 1; i<5; i++){
+    try{
+        if(fs.existsSync(path[i])){
+            console.log(`${path[i]} exists.`)
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
