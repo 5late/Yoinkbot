fe0c158b71ba13d27b2575e0dbc85f15c6aaf28a
5late <christianrfernandes5@gmail.com>
2021-04-22 19:16:12 -0400

    removed for loop

--- test.js
+++ test.js
@@ -6,12 +6,10 @@ const path3 = './package-lock.json'
 const path4 = './build'
 const path5 = './commands'
 
-for (var i = 1; i<5; i++){
-    try{
-        if(fs.existsSync(path1 && path2 && path3 && path4 && path5)){
-            console.log(`The files all exist.`)
-        }
-    } catch (err) {
-        console.log(err)
+try{
+    if(fs.existsSync(path1 && path2 && path3 && path4 && path5)){
+        console.log(`The files all exist.`)
     }
+} catch (err) {
+    console.log(err)
 }
