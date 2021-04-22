4e44d263657ea00df5738a34620f9ddc72b45063
5late <christianrfernandes5@gmail.com>
2021-04-22 19:58:24 -0400

    cleaned up pwrd command

--- commands/misc/password.js
+++ commands/misc/password.js
@@ -1,8 +1,6 @@
 const commando = require('discord.js-commando');
-const fs = require('fs');
-const { DiscordAPIError } = require('discord.js');
-const Discord = require('discord.js')
 const prefix  = '?'
+
 module.exports = class PasswordCommand extends commando.Command {
     constructor(client) {
         super(client, {
@@ -11,8 +9,9 @@ module.exports = class PasswordCommand extends commando.Command {
             memberName: 'password',
             description: 'Need a quick password for a random account? Make a super secure password!',
         })
-    }
-    async run (msg, { text }) {
+}
+
+async run (msg, { text }) {
       const args = msg.content.slice(prefix.length).trim().split(/ +/g);
       const command = args.shift().toLowerCase();
 
@@ -32,27 +31,5 @@ module.exports = class PasswordCommand extends commando.Command {
     }
 
     GeneratePassword();
-        
-        
-        const id = msg.author.id
-        console.log(id)
-        const name = msg.member.user.tag;
-        console.log(name)
-        var information = [];
-        information.push(name, id)
-        var savedinfo = fs.readFileSync("./information.txt", {"encoding": "utf-8"});
-        
-      var newinfo = savedinfo;
-    fs.writeFileSync("information.txt", newinfo.toString())
-    
-    fs.appendFileSync("information.txt", information.toString())
-
-    fs.readFile("./information.txt", function (err, data) {
-        if (err) throw err;
-        var datata = data.toString('utf-8')
-        if(data.includes('Xurxx#7879')){
-         console.log((datata.length / 29) - .689655172413794)
-        }
-      });
     }
 }
