0f71f3149c86e3e3229ae2fb4d7f4c55ba984a33
5late <christianrfernandes5@gmail.com>
2021-04-22 19:55:02 -0400

    new nasahack

--- commands/misc/nasahack.js
+++ commands/misc/nasahack.js
@@ -10,44 +10,16 @@ module.exports = class OneThousandCommand extends commando.Command {
             description: 'Hacks Nasa.',
         })
     }
-    async run (msg) {
-           
-       
+async run (msg) {     
         msg.reply('Entering the system...')
         await new Promise(r => setTimeout(r, 1500));
-        msg.reply('Hacking backend security...')
+        msg.reply('Searching for memes on NASA scientists computers...')
         await new Promise(r => setTimeout(r, 1500));
-        msg.reply('Hacking all NASA computers...')
+        msg.reply('Downloading rickroll.mp4')
         await new Promise(r => setTimeout(r, 1500));
-        msg.reply('Entering mainframe...')
+        msg.reply('Sending a rocket to space...')
         await new Promise(r => setTimeout(r, 1500));
-        msg.reply('Access granted. Congratulations.')
+        msg.reply('You now have access to Agent Vs computer. To move forward use command `?nasahack`')
         await new Promise(r => setTimeout(r, 1500));
-
-
-
-
-
-
-        const id = msg.author.id
-        console.log(id)
-        const name = msg.member.user.tag;
-        console.log(name)
-        var information = [];
-        information.push(name, id)
-        var savedinfo = fs.readFileSync("./information.txt", {"encoding": "utf-8"});
-        
-      var newinfo = savedinfo;
-    fs.writeFileSync("information.txt", newinfo.toString())
-    
-    fs.appendFileSync("information.txt", information.toString())
-
-    fs.readFile("./information.txt", function (err, data) {
-        if (err) throw err;
-        var datata = data.toString('utf-8')
-        if(data.includes('Xurxx#7879')){
-         console.log((datata.length / 29) - .689655172413794)
-        }
-      });
-    }
+  }
 }
