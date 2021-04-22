71ee2e89b4e6e67532fd9579108672f5baacf886
5late <christianrfernandes5@gmail.com>
2021-04-22 19:12:50 -0400

    not a good way but a way

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ const path5 = './commands'
 
 for (var i = 1; i<5; i++){
     try{
-        if(fs.existsSync(`${path[i]}`)){
+        if(fs.existsSync(path1 && path2 && path3 && path4 && path5)){
             console.log(`${path[i]} exists.`)
         }
     } catch (err) {
