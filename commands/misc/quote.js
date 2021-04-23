240bdeaa98cc0ee62c235ea05cd60f0a76b77014
5late <christianrfernandes5@gmail.com>
2021-04-22 20:03:36 -0400

    quote message now returns year as well

--- commands/misc/quote.js
+++ commands/misc/quote.js
@@ -23,34 +23,8 @@ module.exports = class QuoteCommand extends commando.Command {
         const command = args.shift().toLowerCase();
         let rMember = msg.guild.member(msg.mentions.users.first())// || msg.guild.members.cache.get(args[0]))
 
-
-
-        
-        //let finalMessage = text - args[0]
         let finalMessages = await text.replace(args[0], '')
-        //msg.channel.send(finalMessage)
-        await msg.channel.send(finalMessages + `\n-${rMember}`)
 
-
-        const id = msg.author.id
-        console.log(id)
-        const name = msg.member.user.tag;
-        console.log(name)
-        var information = [];
-        information.push(name, id)
-        var savedinfo = fs.readFileSync("./information.txt", {"encoding": "utf-8"});
-        
-      var newinfo = savedinfo;
-    fs.writeFileSync("information.txt", newinfo.toString())
-    
-    fs.appendFileSync("information.txt", information.toString())
-
-    fs.readFile("./information.txt", function (err, data) {
-        if (err) throw err;
-        var datata = data.toString('utf-8')
-        if(data.includes('Xurxx#7879')){
-         console.log((datata.length / 29) - .689655172413794)
-        }
-      });
+        await msg.channel.send(finalMessages + `\n-${rMember}, ${new Date().getFullYear()}`)
     }
 }
