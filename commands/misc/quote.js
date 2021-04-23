30990140a49a669d029f43f4bab0c3460bc8ae0e
5late <christianrfernandes5@gmail.com>
2021-04-23 12:15:29 -0400

    cleaned up

--- commands/misc/quote.js
+++ commands/misc/quote.js
@@ -27,42 +27,13 @@ async run (msg, { text }) {
         const args = msg.content.slice(prefix.length).trim().split(/ +/g);
         const command = args.shift().toLowerCase();
         let rMember = msg.guild.member(msg.mentions.users.first())
-        function getIdFromMention(mention) {
-            if (!mention) return;
-    
-            if (mention.startsWith('<@') && mention.endsWith('>')) {
-                mention = mention.slice(2, -1);
-                
-                if (mention.startsWith('!')) {
-                    mention = mention.slice(1);
-                }
-        
-            }
-            mention = client.users.fetch(mention)
-            return mention;
-        }
-        function getContentFromID(ID) {
-            let channel = msg.channel.id
-            ID = client.channels.cache.get(channel)
-            return ID;
-        }
 
         if(args.length > 1){
             let finalMessages = await text.replace(args[0], '')
 
             await msg.channel.send(finalMessages + `\n-${rMember}, ${new Date().getFullYear()}`)
         } else {
-            try {
-                const user = await getIdFromMention(args[0]);
-                if (!user) return msg.channel.send('Oh no, invalid user ID.');
-              
-                const lastMessage = getContentFromID(rMember.lastMessageID)
-                if (!lastMessage) return msg.channel.send('No messages from this user found.');
-              
-                msg.channel.send(lastMessage.content, lastMessage.embed, lastMessage.attachments);
-              } catch(err) {
-                console.error(err);
-              }
+            msg.channel.send(`You haven't given me anything to quote from this user: ${args[0]}`)
         }
     }
 }
