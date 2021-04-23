1dec9b5d83331a8aac923899041fa2adb7549830
5late <christianrfernandes5@gmail.com>
2021-04-23 12:31:26 -0400

    garbage removal

--- commands/misc/say.js
+++ commands/misc/say.js
@@ -23,26 +23,5 @@ module.exports = class OneThousandCommand extends commando.Command {
         const command = args.shift().toLowerCase();
 
         msg.channel.send(text).then(msg.delete())
-
-        const id = msg.author.id
-        console.log(id)
-        const name = msg.member.user.tag;
-        console.log(name)
-        var information = [];
-        information.push(name, id)
-        var savedinfo = fs.readFileSync("./information.txt", {"encoding": "utf-8"});
-        
-      var newinfo = savedinfo;
-    fs.writeFileSync("information.txt", newinfo.toString())
-    
-    fs.appendFileSync("information.txt", information.toString())
-
-    fs.readFile("./information.txt", function (err, data) {
-        if (err) throw err;
-        var datata = data.toString('utf-8')
-        if(data.includes('Xurxx#7879')){
-         console.log((datata.length / 29) - .689655172413794)
-        }
-      });
     }
 }
