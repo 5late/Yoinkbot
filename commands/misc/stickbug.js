21fbbfb982e35fa8fb933cff0236fc103a33c182
5late <christianrfernandes5@gmail.com>
2021-04-23 12:36:55 -0400

    cleaned up

--- commands/misc/stickbug.js
+++ commands/misc/stickbug.js
@@ -1,10 +1,6 @@
 const commando = require('discord.js-commando');
-const fs = require('fs');
-const Discord = require('discord.js')
-const { DiscordAPIError } = require('discord.js');
 
-
-module.exports = class OneThousandCommand extends commando.Command {
+module.exports = class StickBugCommand extends commando.Command {
     constructor(client) {
         super(client, {
             name: 'stickbuglol',
@@ -15,30 +11,11 @@ module.exports = class OneThousandCommand extends commando.Command {
             
         })
       }
-    async run (msg) {
+
+async run (msg) {
            
-       msg.delete()
-msg.channel.send('https://i.imgur.com/mfKEG74.gif?noredirect')
+      msg.delete()
+      msg.channel.send('https://i.imgur.com/mfKEG74.gif?noredirect')
 
-        const id = msg.author.id
-        console.log(id)
-        const name = msg.member.user.tag;
-        console.log(name)
-        var information = [];
-        information.push(name, id)
-        var savedinfo = fs.readFileSync("./information.txt", {"encoding": "utf-8"});
-        
-      var newinfo = savedinfo;
-    fs.writeFileSync("information.txt", newinfo.toString())
-    
-    fs.appendFileSync("information.txt", information.toString())
-
-    fs.readFile("./information.txt", function (err, data) {
-        if (err) throw err;
-        var datata = data.toString('utf-8')
-        if(data.includes('Xurxx#7879')){
-         console.log((datata.length / 29) - .689655172413794)
-        }
-      });
     }
 }
