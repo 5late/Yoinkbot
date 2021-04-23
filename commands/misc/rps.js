aa8981c9bf909c91c741c0e65c1e71196ff8f1b9
5late <christianrfernandes5@gmail.com>
2021-04-22 20:07:42 -0400

    garbage remocal

--- commands/misc/rps.js
+++ commands/misc/rps.js
@@ -68,28 +68,7 @@ module.exports = class RPSCommand extends commando.Command {
         fs.writeFileSync("rpsbotwon.txt", botwincounter.toString());
         console.log('Yoinkbot wins: ' + botwin.length)
         msg.channel.send('Ha! Yoinkbot has won ' + botwin.length + ' times!')
-                }
-        const id = msg.author.id
-        console.log(id)
-        const name = msg.member.user.tag;
-        console.log(name)
-        var information = [];
-        information.push(name, id)
-        var savedinfo = fs.readFileSync("./information.txt", {"encoding": "utf-8"});
-        
-      var newinfo = savedinfo;
-    fs.writeFileSync("information.txt", newinfo.toString())
-    
-    fs.appendFileSync("information.txt", information.toString())
-
-    fs.readFile("./information.txt", function (err, data) {
-        if (err) throw err;
-        var datata = data.toString('utf-8')
-        if(data.includes('Xurxx#7879')){
-         console.log((datata.length / 29) - .689655172413794)
-        }
-      });
-                
     }
-    
+                
+}    
 }   
