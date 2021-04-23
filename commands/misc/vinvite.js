abb095dcd0916ffa7544168af18fd850dce340d7
5late <christianrfernandes5@gmail.com>
2021-04-23 12:54:28 -0400

    garbage removal

--- commands/misc/vinvite.js
+++ commands/misc/vinvite.js
@@ -19,29 +19,8 @@ module.exports = class OneThousandCommand extends commando.Command {
       
       let voiceChannel = msg.member.voice.channel;
 
-        let invite = await msg.channel.createInvite()
+        let invite = await msg.voiceChannel.createInvite()
 
         msg.channel.send(invite)
-        
-        const id = msg.author.id
-        console.log(id)
-        const name = msg.member.user.tag;
-        console.log(name)
-        var information = [];
-        information.push(name, id)
-        var savedinfo = fs.readFileSync("./information.txt", {"encoding": "utf-8"});
-        
-      var newinfo = savedinfo;
-    fs.writeFileSync("information.txt", newinfo.toString())
-    
-    fs.appendFileSync("information.txt", information.toString())
-
-    fs.readFile("./information.txt", function (err, data) {
-        if (err) throw err;
-        var datata = data.toString('utf-8')
-        if(data.includes('Xurxx#7879')){
-         console.log((datata.length / 29) - .689655172413794)
-        }
-      });
     }
 }
