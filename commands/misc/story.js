31a1bf66b9a165651eceb1b69d61a250e88b4217
5late <christianrfernandes5@gmail.com>
2021-04-23 12:37:40 -0400

    garbage removal/cleaned up

--- commands/misc/story.js
+++ commands/misc/story.js
@@ -1,8 +1,7 @@
 const commando = require('discord.js-commando');
-const fs = require('fs');
-const { DiscordAPIError } = require('discord.js');
 const Discord = require('discord.js')
 const prefix  = '?'
+
 module.exports = class StoryCommand extends commando.Command {
     constructor(client) {
         super(client, {
@@ -19,7 +18,8 @@ module.exports = class StoryCommand extends commando.Command {
           ]
         })
     }
-    async run (msg, { text }) {
+
+async run (msg) {
       const args = msg.content.slice(prefix.length).trim().split(/ +/g);
       const command = args.shift().toLowerCase();
       let ruser = msg.guild.member(msg.mentions.users.first() || msg.guild.members.cache.get(args[0]))
@@ -68,27 +68,5 @@ const embed = new Discord.MessageEmbed()
 .setFooter(`Yoinkbot may collect some data. Use command ?owner to contact the owner to learn more.`)
 
 msg.channel.send(embed)
-        
-        
-        const id = msg.author.id
-        console.log(id)
-        const name = msg.member.user.tag;
-        console.log(name)
-        var information = [];
-        information.push(name, id)
-        var savedinfo = fs.readFileSync("./information.txt", {"encoding": "utf-8"});
-        
-      var newinfo = savedinfo;
-    fs.writeFileSync("information.txt", newinfo.toString())
-    
-    fs.appendFileSync("information.txt", information.toString())
-
-    fs.readFile("./information.txt", function (err, data) {
-        if (err) throw err;
-        var datata = data.toString('utf-8')
-        if(data.includes('Xurxx#7879')){
-         console.log((datata.length / 29) - .689655172413794)
-        }
-      });
     }
 }
