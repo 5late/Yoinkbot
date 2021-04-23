f24dd3e98a5673e0f3f0901d5eb342a5bb102b11
5late <christianrfernandes5@gmail.com>
2021-04-23 12:46:25 -0400

    wip uwu

--- commands/misc/uwu.js
+++ commands/misc/uwu.js
@@ -1,8 +1,6 @@
 const commando = require('discord.js-commando');
-const fs = require('fs');
-const { DiscordAPIError } = require('discord.js');
-const Discord = require('discord.js')
 const prefix  = '?'
+
 module.exports = class OneThousandCommand extends commando.Command {
     constructor(client) {
         super(client, {
@@ -10,35 +8,49 @@ module.exports = class OneThousandCommand extends commando.Command {
             group: 'misc',
             memberName: 'uwu',
             description: 'uwu',
+            args:[
+              { 
+                  key:'text',
+                  prompt: 'Who would you like to be the main character in this story?',
+                  type: 'string',
+              }
+          ]
         })
     }
     async run (msg, { text }) {
+      var finalMessage = [];
+      
       const args = msg.content.slice(prefix.length).trim().split(/ +/g);
       const command = args.shift().toLowerCase();
-
-
-msg.channel.send("uwu " + args)
-
         
-        const id = msg.author.id
-        console.log(id)
-        const name = msg.member.user.tag;
-        console.log(name)
-        var information = [];
-        information.push(name, id)
-        var savedinfo = fs.readFileSync("./information.txt", {"encoding": "utf-8"});
-        
-      var newinfo = savedinfo;
-    fs.writeFileSync("information.txt", newinfo.toString())
-    
-    fs.appendFileSync("information.txt", information.toString())
+      let uwuaters = ["h", "c"]
 
-    fs.readFile("./information.txt", function (err, data) {
-        if (err) throw err;
-        var datata = data.toString('utf-8')
-        if(data.includes('Xurxx#7879')){
-         console.log((datata.length / 29) - .689655172413794)
+      if(text.length > 91) {
+        msg.channel.send('Keep it under 91 characters!')
+      } 
+      else{
+
+        msg.delete()
+        for(var i = 0; i < text.length; i++)
+
+      {
+        let newText = text.toLowerCase()
+        if (newText[i].indexOf(' ') != -1){
+          
+          finalMessage.push(' ')
+
+        } else if (uwuaters.includes(newText[i])) {
+
+          finalMessage.push(`w`)
+
+        } else{
+
+        finalMessage.push(`${newText[i]}`)
+
         }
-      });
+
+      }
+      await msg.channel.send(finalMessage.join(" "))
     }
+  }
 }
